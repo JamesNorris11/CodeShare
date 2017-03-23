dfa3fd80cfa7d7e589a573647be7d031459dac01
JamesNorris11 <jpnorris21@gmail.com>
2017-03-23 18:03:33 +0000

    added register page

---
+++ paste.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: James Norris
+ * Date: 20/03/2017
+ * Time: 14:47
+ */
+
+require_once('CS.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CodeShare</title>
+    <meta name="description" content="CodeShare">
+    <meta name="author" content="JN">
+
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/paste.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+</head>
+<body>
+<header>
+    <a id="bannerLink" href="index.php"><span class="dots">&bull;</span> CodeShare <span class="dots">&bull;</span></a>
+
+    <ul id="navList">
+        <li class="navButton"><a href="" id="active">New Upload</a></li>
+        <li class="navButton"><a href="search.php">Search</a></li>
+        <li class="navButton"><a href="login.php">Login</a></li>
+        <li class="navButton"><a href="register.php">Register</a></li>
+        <li class="navButton"><a href="logout.php">Logout</a></li>
+    </ul>
+</header>
+<main>
+    <aside>
+        <span class="title stats">Total Users</span>
+        <span class="number stats"><?php echo number_format(CS::getStats('Jds3f')['users']); ?></span>
+        <span class="title stats">Total Posts</span>
+        <span class="number stats"><?php echo number_format(CS::getStats('Jds3f')['posts']); ?></span>
+
+        <img src="images/notebook.jpg" id="noteBookImage" />
+    </aside>
+    <section>
+        <form action="add.php" method="post" id="addPostForm" name="mainForm">
+
+            <label for="content" class="labelAbove">Text to Upload</label>
+            <textarea name="content" class="textarea" id="content" cols=""></textarea>
+
+            <label for="description" class="labelAbove">Description</label>
+            <textarea id="description" name="description" cols="" class="textarea"></textarea>
+
+            <label for="password">Post Password</label>
+            <input type="Password" class="input horizontalInput" id="password" name="password">
+
+            <label for="language">Syntax Highlighting</label>
+            <select class="input horizontalInput" name="language">
+                <option label="abap" value="abap">abap</option>
+                <option label="actionscript" value="actionscript" selected="selected">actionscript</option>
+                <option label="actionscript3" value="actionscript3">actionscript3</option>
+            </select>
+
+            <label for="language">Captcha</label>
+            <!-- <img src="captcha.php" /> -->
+            <input type="text" class="input horizontalInput" id="security" name="security">
+            <input type="submit" name="submit" class="submit" value="Submit"/>
+        </form>
+    </section>
+</main>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: James Norris
+ * Date: 20/03/2017
+ * Time: 14:47
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CodeShare</title>
+    <meta name="description" content="CodeShare">
+    <meta name="author" content="JN">
+
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/register.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+</head>
+<body>
+<header>
+    <a id="bannerLink" href="index.php"><span class="dots">&bull;</span> CodeShare <span class="dots">&bull;</span></a>
+
+    <ul id="navList">
+        <li class="navButton"><a href="index.php">New Upload</a></li>
+        <li class="navButton"><a href="search.php">Search</a></li>
+        <li class="navButton"><a href="login.php">Login</a></li>
+        <li class="navButton"><a href="register.php" id="active">Register</a></li>
+    </ul>
+</header>
+<main>
+    <aside>
+    </aside>
+    <section>
+        <form action="checkregister.php" method="post" class="detailsForm" name="registerForm">
+            <span class="titlePhrase">Register</span>
+
+            <div>
+                <label for="username">Email</label>
+                <input name="username" type="text" class="input field">
+                <span class="help">?</span>
+            </div>
+            <div>
+                <label for="displayName">Display Name</label>
+                <input name="displayName" type="text" class="input field">
+                <span class="help">?</span>
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input name="password" type="password" class="input field">
+                <span class="help">?</span>
+            </div>
+            <div>
+                <label for="repeatPassword">Repeat Password</label>
+                <input name="repeatPassword" type="password" class="input field">
+                <span class="help">?</span>
+            </div>
+            <input type="submit" name="Submit" class="submit" value="Register">
+        </form>
+    </section>
+</main>
+<script src="js/script.js"></script>
+</body>
+</html>
